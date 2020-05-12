ccb2e3bdcd7c3644106f1a86a153cdf626ed281e
Lucas-Werner <lucas.1werner@gmail.com>
2020-05-12 17:23:45 -0300

    state feito

--- src/pages/ShoppingCartPage.js
+++ src/pages/ShoppingCartPage.js
@@ -1,2 +1,25 @@
-// iniciando shopping cart page
-import React from 'react;
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import './ShoppingCart.css';
+import emptyCart from '../imgs/carrinho-vazio.jpg';
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    const teste = JSON.parse(localStorage.getItem('products'));    
+    this.state = {
+      isShouldRedirect: false,
+      redirectPage: '',
+      productsArr: teste,
+    };
+    this.removeFromCart = this.removeFromCart.bind(this);
+    this.changeQuantity = this.changeQuantity.bind(this);
+    this.onChangeRedirect = this.onChangeRedirect.bind(this);
+  }
+
+  onChangeRedirect(string) {
+    this.setState({
+      isShouldRedirect: true,
+      redirectPage: string,
+    });
+  }
