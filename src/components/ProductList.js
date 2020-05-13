59389da2164828c5378e78340741568961216586
DevT <aguiarthiag@gmail.com>
2020-05-13 16:12:10 -0300

    CC

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,28 +2,22 @@ import React, { Component } from 'react';
 import '../pages/PLP.css';
 
 class ProductList extends Component {
-  constructor(props){
-    super (props);
-
-  }
-
   render() {
-    const { list } = this.props
+    const { list } = this.props;
 
-    if(list.length>0){
-      return(
+    if (list.length > 0) {
+      return (
         <div className="product-list">
           {list.map((card) => <p data-testid="product">{card.title}</p>)}
         </div>
-      )
+      );
     }
   
-    return( 
-    <div data-testid="home-initial-message" className="product-list">
-      Digite algum termo de pesquisa ou escolha uma categoria.
-    </div>
-    )
-    
+    return (
+      <div data-testid="home-initial-message" className="product-list">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </div>
+    );
   }
 }
 
--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import * as API from '../services/api';
-import { Link } from 'react-router-dom';
 import './PLP.css';
 import ProductList from '../components/ProductList';
 
 class ProductListPage extends Component {
-  constructor(props){
-    super (props);
+  constructor(props) {
+    super(props);
 
     this.state = {
       categories: [],
       query: '',
-      categoryID:'',
+      categoryID: '',
       list: [],
-    }
+    };
     this.handleRadio = this.handleRadio.bind(this);
     this.handleQueryButton = this.handleQueryButton.bind(this);
   }
@@ -23,49 +22,47 @@ class ProductListPage extends Component {
   }
 
   categoryList(id, name) {
-    return(
+    return (
       <div key={id}>
         <input 
-          type="radio" id={id} value={id} 
-          onClick={(e) => this.handleRadio(e)} 
-          name="category" key={id} 
+          type="radio" id={id} value={id}
+          onClick={(e) => this.handleRadio(e)}
+          name="category" key={id}
         />
         <label data-testid="category" htmlFor={id}>{name}</label>
       </div>
-    )
+    );
   }
 
   handleRadio(event) {
     const { query } = this.state;
-    this.setState({ categoryID: event.target.value});
+    this.setState({ categoryID: event.target.value });
     if (query) {
       API.getQueryNCategory(event.target.value, query)
-        .then((r) => this.setState({ list: r.results }))
+        .then((r) => this.setState({ list: r.results }));
+    } else {
+      API.getCategory(event.target.value).then((r) => this.setState({ list: r.results }));
     }
-    else {
-      API.getCategory(event.target.value).then((r) => this.setState({ list: r.results }))
-    }
-    
   }
 
   handleQueryButton() {
     const { list, query, categoryID } = this.state;
-    if(list.length > 0) {
-      API.getQueryNCategory(categoryID, query).then((r) => this.setState({ list: r.results }))
-    }
-    else {
-      API.getQuery(query).then((r) => this.setState({ list: r.results }))
+    if (list.length > 0) {
+      API.getQueryNCategory(categoryID, query).then((r) => this.setState({ list: r.results }));
+    } else {
+      API.getQuery(query).then((r) => this.setState({ list: r.results }));
     }
   }
 
 
   render() {
-    const { categories, query } = this.state
-    return(
+    const { categories, query } = this.state;
+    return (
       <div className="container">
         <div className="header">
-          <input type="text" data-testid="query-input" value={query} 
-            onChange={(e) => this.setState({ query: e.target.value})} 
+          <input 
+            type="text" data-testid="query-input" value={query}
+            onChange={(e) => this.setState({ query: e.target.value })}
           />
           <button type="button" data-testid="query-button" onClick={this.handleQueryButton}>
             BUSCA
@@ -78,7 +75,7 @@ class ProductListPage extends Component {
           <ProductList list={this.state.list} />
         </div>
       </div>
-    )
+    );
   }
 
 }
