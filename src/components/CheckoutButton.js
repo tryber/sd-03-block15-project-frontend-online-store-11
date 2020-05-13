b51801cbb03fba086a5c596c8b057552809cd6cb
renan.andreolle <renandreolle@hotmail.com>
2020-05-13 20:36:11 -0300

    CC2

--- src/components/CheckoutButton.js
+++ src/components/CheckoutButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class CheckoutButton extends React.Component {
-  render(){
+  render() {
     return null;
   }
 }
--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -3,39 +3,47 @@ import React from 'react';
 class CheckoutForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = { value: '' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
-  handleSubmit(event) {
+  static handleSubmit(event) {
     event.preventDefault();
   }
 
-  render(){
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.value}
-         onChange={this.handleChange} name="fullName" placeholder="Nome Completo"/>
-        <input type="text" value={this.state.value}
-         onChange={this.handleChange} name="CPF" placeholder="CPF"/>
-        <input type="email" value={this.state.value}
-         onChange={this.handleChange} name="email" placeholder="email"/>
-        <input type="text" value={this.state.value}
-         onChange={this.handleChange} name="Phone" placeholder="Telefone"/>
-        <input type="text" value={this.state.value}
-         onChange={this.handleChange} name="fCEP" placeholder="CEP"/>
-        <input type="text" value={this.state.value}
-         onChange={this.handleChange} name="Address" placeholder="Endereço"/>
-        <input type="text" value={this.state.value}
-         onChange={this.handleChange} name="Complemento" placeholder="Complemento"/>
-        <input type="text" value={this.state.value}
-         onChange={this.handleChange} name="Numero" placeholder="Numero"/>
+        <input
+          type="text" value={this.state.value}
+          onChange={this.handleChange} name="fullName" placeholder="Nome Completo" />
+        <input
+          type="text" value={this.state.value}
+          onChange={this.handleChange} name="CPF" placeholder="CPF" />
+        <input
+          type="email" value={this.state.value}
+          onChange={this.handleChange} name="email" placeholder="email" />
+        <input
+          type="text" value={this.state.value}
+          onChange={this.handleChange} name="Phone" placeholder="Telefone" />
+        <input
+          type="text" value={this.state.value}
+          onChange={this.handleChange} name="fCEP" placeholder="CEP" />
+        <input
+          type="text" value={this.state.value}
+          onChange={this.handleChange} name="Address" placeholder="Endereço" />
+        <input
+          type="text" value={this.state.value}
+          onChange={this.handleChange} name="Complemento" placeholder="Complemento" />
+        <input
+          type="text" value={this.state.value}
+          onChange={this.handleChange} name="Numero" placeholder="Numero" />
       </form>
     );
   }
