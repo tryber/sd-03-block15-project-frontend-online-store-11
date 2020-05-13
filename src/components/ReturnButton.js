09e4f391e6bde8af0e4ce6679539b34b8b478473
renan.andreolle <renandreolle@hotmail.com>
2020-05-13 18:41:55 -0300

    Feito return button

--- src/components/ReturnButton.js
+++ src/components/ReturnButton.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUndo } from '@fortawesome/free-solid-svg-icons';
 
 class ReturnButton extends React.Component {
   render(){
-    return null;
+    return <Link to="/"><FontAwesomeIcon icon={faUndo} color="black"/></Link>;
   }
 }
 
