9928b51ee47e532354b9d4221185b1b56d1eca51
renan.andreolle <renandreolle@hotmail.com>
2020-05-14 03:08:09 -0300

    Pagamento feito

--- src/components/CheckoutPay.js
+++ src/components/CheckoutPay.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
 class CheckoutPay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {checked: ''};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({checked: event.target.value});
+  }
+
   render() {
-    return null;
+    return (
+      <div>
+        <p>Método de Pagamento</p>
+        <label> Boleto
+          <input
+            type="radio" value="Boleto" checked={this.state.checked === 'Boleto'}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label> Cartão de Crédito
+        <input
+          type="radio" value="Visa" checked={this.state.checked === 'Visa'}
+          onChange={this.handleChange}
+        />Visa
+        <input
+          type="radio" value="Mastercard" checked={this.state.checked === 'Mastercard'}
+          onChange={this.handleChange}
+        />Mastercard
+        <input
+          type="radio" value="Elo" checked={this.state.checked === 'Elo'}
+          onChange={this.handleChange}
+        />Elo
+        </label>
+      </div>
+    );
   }
 }
 
