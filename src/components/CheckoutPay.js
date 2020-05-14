e8c39f362c7f56049f0c628da3ad732833dbfa1c
renan.andreolle <renandreolle@hotmail.com>
2020-05-14 03:15:11 -0300

    CC9

--- src/components/CheckoutPay.js
+++ src/components/CheckoutPay.js
@@ -3,25 +3,25 @@ import React from 'react';
 class CheckoutPay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {checked: ''};
+    this.state = { checked: '' };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({checked: event.target.value});
+    this.setState({ checked: event.target.value });
   }
 
   render() {
     return (
       <div>
         <p>Método de Pagamento</p>
-        <label> Boleto
+        <label htmlFor="Boleto"> Boleto
           <input
             type="radio" value="Boleto" checked={this.state.checked === 'Boleto'}
             onChange={this.handleChange}
           />
         </label>
-        <label> Cartão de Crédito
+        <label htmlFor="Visa"> Cartão de Crédito
         <input
           type="radio" value="Visa" checked={this.state.checked === 'Visa'}
           onChange={this.handleChange}
