fb62e08322aae6fcd23f7384e8f3669b060721d8
renan.andreolle <renandreolle@hotmail.com>
2020-05-13 21:17:12 -0300

    CC4

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -18,43 +18,32 @@ class CheckoutForm extends React.Component {
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text" value={this.state.value}
-          onChange={this.handleChange} name="fullName" placeholder="Nome Completo"
-        />
-        <input
-          type="text" value={this.state.value}
-          onChange={this.handleChange} name="CPF" placeholder="CPF"
-        />
-        <input
-          type="email" value={this.state.value}
-          onChange={this.handleChange} name="email" placeholder="email"
-        />
-        <input
-          type="text" value={this.state.value}
-          onChange={this.handleChange} name="Phone" placeholder="Telefone"
-        />
-        <input
-          type="text" value={this.state.value}
-          onChange={this.handleChange} name="fCEP" placeholder="CEP"
-        />
-        <input
-          type="text" value={this.state.value}
-          onChange={this.handleChange} name="Address" placeholder="Endereço"
-        />
-        <input
-          type="text" value={this.state.value}
-          onChange={this.handleChange} name="Complemento" placeholder="Complemento"
-        />
-        <input
-          type="text" value={this.state.value}
-          onChange={this.handleChange} name="Numero" placeholder="Numero"
-        />
-      </form>
-    );
-  }
+    return (<form onSubmit={this.handleSubmit}>
+      <input type="text" value={this.state.value} onChange={this.handleChange}
+        name="fullName" placeholder="Nome Completo"
+      />
+      <input type="text" value={this.state.value}
+        onChange={this.handleChange} name="CPF" placeholder="CPF"
+      />
+      <input type="email" value={this.state.value}
+        onChange={this.handleChange} name="email" placeholder="email"
+      />
+      <input type="text" value={this.state.value}
+        onChange={this.handleChange} name="Phone" placeholder="Telefone"
+      />
+      <input type="text" value={this.state.value}
+        onChange={this.handleChange} name="fCEP" placeholder="CEP"
+      />
+      <input type="text" value={this.state.value}
+        onChange={this.handleChange} name="Address" placeholder="Endereço"
+      />
+      <input type="text" value={this.state.value}
+        onChange={this.handleChange} name="Complemento" placeholder="Complemento"
+      />
+      <input type="text" value={this.state.value}
+        onChange={this.handleChange} name="Numero" placeholder="Numero"
+      />
+    </form>);}
 }
 
 export default CheckoutForm;
--- src/components/ReturnButton.js
+++ src/components/ReturnButton.js
@@ -5,7 +5,7 @@ import { faUndo } from '@fortawesome/free-solid-svg-icons';
 
 class ReturnButton extends React.Component {
   render() {
-    return <Link to="/"><FontAwesomeIcon icon={faUndo} color="black" /></ Link>;
+    return <Link to="/"><FontAwesomeIcon icon={faUndo} color="black" /></Link>;
   }
 }
 
